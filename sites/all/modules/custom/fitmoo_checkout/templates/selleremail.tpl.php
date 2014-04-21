165de7a25b05e8649c5be43724a4f457a04da13f
George Plsek <george@gtekg.com>
2014-04-21 07:57:43 -0700

    add email template

---
+++ sites/all/modules/custom/fitmoo_checkout/templates/selleremail.tpl.php
@@ -0,0 +1,8 @@
+Good news! Someone purchased <?php print $product;?>. For details please visit <a href="https://beta.fitmoo.com/dashboard/sales">Your Sales</a> at Fitmoo.com
+
+
+You have received an order for <?php print $product;?>
+ #<?php print $orderID;?> 
+ on fitmoo.com from 
+ <?php print $buyerEmail;?>
+Login to your Fitmoo.com account go to dashboard, select the sales tab to view the order details
