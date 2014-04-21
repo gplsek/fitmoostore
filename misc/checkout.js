f47ea9da425487c902ba7424b3ca3b132253907c
Brian Anarell <brian.anarell@gmail.com>
2014-04-21 09:31:23 -0700

    updated checkout css for floating form blocks right

--- misc/checkout.js
+++ misc/checkout.js
@@ -1,16 +1,16 @@
-$ = jQuery.noConflict();
-$(document).ready(function(){
-    var m = $("#edit-commerce-payment");
-    $("#customer-profile-shipping-ajax-wrapper").append(m);
-    $("#edit-commerce-payment").css('width','100%');
-    $("#edit-commerce-payment, #customer-profile-billing-ajax-wrapper").css('border-right','0px');
-    $("#edit-commerce-payment").css('padding','10px 0px 0px 0px');
-    m = $("#edit-buttons");
-    $("#customer-profile-shipping-ajax-wrapper").append(m);
-    $("#edit-buttons").css('width','100%');
-    $("#customer-profile-shipping-ajax-wrapper").css('height','auto');
+// $ = jQuery.noConflict();
+// $(document).ready(function(){
+//     var m = $("#edit-commerce-payment");
+//     $("#customer-profile-shipping-ajax-wrapper").append(m);
+//     $("#edit-commerce-payment").css('width','100%');
+//     $("#edit-commerce-payment, #customer-profile-billing-ajax-wrapper").css('border-right','0px');
+//     $("#edit-commerce-payment").css('padding','10px 0px 0px 0px');
+//     m = $("#edit-buttons");
+//     $("#customer-profile-shipping-ajax-wrapper").append(m);
+//     $("#edit-buttons").css('width','100%');
+//     $("#customer-profile-shipping-ajax-wrapper").css('height','auto');
 
-});
+// });
 
 
 
