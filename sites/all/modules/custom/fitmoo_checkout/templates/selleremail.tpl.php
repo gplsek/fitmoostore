9846134c1bd01969b9f91dfe8914f5a565a6d693
George Plsek <george@gtekg.com>
2014-04-21 10:09:52 -0700

    update params

--- sites/all/modules/custom/fitmoo_checkout/templates/selleremail.tpl.php
+++ sites/all/modules/custom/fitmoo_checkout/templates/selleremail.tpl.php
@@ -1,8 +1,8 @@
-<p>Good news! Someone purchased <?php print $product;?>. For details please visit <a href="https://beta.fitmoo.com/dashboard/sales">Your Sales</a> at Fitmoo.com</p>
+<p>Good news! Someone purchased <?php print $product;?>. For details please visit <a href="https://beta.fitmoo.com/dashboard/sales">Your Sales</a> at Fitmoo.com. We will cancel this transaction if the item is not shipped within <strong>5days</strong></p>
 <h1>Order Details</h1>
 
 Order #: <?php print $orderID;?> 
 Placed on: <?php print gmdate("Y-m-d\TH:i:s\Z", $orderDate);?> 
 
 
-<?php print views_embed_view('commerce_cart_summary','block_3', 65); ?>
+<?php print views_embed_view('commerce_cart_summary','block_2', 65); ?>
