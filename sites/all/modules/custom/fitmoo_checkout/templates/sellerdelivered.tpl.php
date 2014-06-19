4ef60743c104a2f25f4a0c7c307445e95333e792
Max Klimchuk <sabre@tut.by>
2014-06-19 22:53:27 +0300

    mails

--- sites/all/modules/custom/fitmoo_checkout/templates/sellerdelivered.tpl.php
+++ sites/all/modules/custom/fitmoo_checkout/templates/sellerdelivered.tpl.php
@@ -272,7 +272,7 @@ $fitfeeperc = $buyperc + $fitperc;
                 <div class="content-div" style="padding-top: 20px;">
                 <h1>Order Details</h1>
                     <div style="float: left;">Order #: <a href="<?php print $redirect_base;?>/dashboard/sales/<?php print $orderID;?>"><?php print $orderID;?></a></div>
-                    <div style="float: right;">Placed on: <?php print date("m-d-Y", $orderDate);?></div>
+                    <div style="float: right;">Placed on: <?php print date("M jS, Y", $orderDate);?></div>
                 </div>
 
             </div>
