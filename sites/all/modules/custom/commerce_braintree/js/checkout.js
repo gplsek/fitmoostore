36d45204d3651c60498d539b61e9a9c3fd2eb5e9
Max Klimchuk <sabre@tut.by>
2014-06-23 22:04:02 +0300

    checkout form

--- sites/all/modules/custom/commerce_braintree/js/checkout.js
+++ sites/all/modules/custom/commerce_braintree/js/checkout.js
@@ -1,9 +1,40 @@
 $ = jQuery.noConflict();
+
+
+
 $(document).ready(function(){
+
+    $("#edit-customer-profile-shipping-field-phone").css('display','none');
+    $("#edit-customer-profile-shipping-commerce-customer-address").css('display','none');
+    var temp = $("#edit-customer-profile-shipping > legend >span");
+    temp.addClass("fieldset_collapsed");
 
     $("#edit-number").attr("disabled",true).css("background-color","rgb(235, 235, 228)");
     $("#edit-owner").attr("disabled",true).css("background-color","rgb(235, 235, 228)");
     $("#edit-code").attr("disabled",true).css("background-color","rgb(235, 235, 228)");
+    var viewform = false;
+
+    var switchdisplay = function( ) {
+        if (viewform == false) {
+            $("#edit-customer-profile-shipping-field-phone").css('display','block');
+            $("#edit-customer-profile-shipping-commerce-customer-address").css('display','block');
+            temp.removeClass("fieldset_collapsed");
+            temp.addClass("fieldset-expanded");
+            viewform = true;
+        } else {
+            $("#edit-customer-profile-shipping-field-phone").css('display','none');
+            $("#edit-customer-profile-shipping-commerce-customer-address").css('display','none');
+            temp.addClass("fieldset_collapsed");
+            temp.removeClass("fieldset-expanded");
+            viewform = false;
+        }
+    };
+
+    temp.click(function () {
+        switchdisplay()
+    });
+
+
 
     $(".form-radio").change(function () {
         var val = $(this).val();
